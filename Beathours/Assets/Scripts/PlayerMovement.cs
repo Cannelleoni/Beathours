526143b71951ebaf2e5dea85cc3b6d41f9c31612
Leon Portius <Pluto-is-not-too-small@gmx.de>
2019-11-08 15:28:53 +0100

    Made test level & walking

--- Beathours/Assets/Scripts/PlayerMovement.cs
+++ Beathours/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float speed;
+
+
+    private void Update()
     {
-        
+        // rechts
+        if(InputManager.mainJoyStick() != null)
+        {
+            rb.MovePosition(transform.position + InputManager.mainJoyStick() * Time.deltaTime * speed);
+        } 
     }
 
 
     //hi
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
+
 }
