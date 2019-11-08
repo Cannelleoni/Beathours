21dd5ca909c28d3475abc319b633917897457384
Leon Portius <Pluto-is-not-too-small@gmx.de>
2019-11-08 19:43:21 +0100

    tile pathfinding & model implementation

---
+++ Beathours/Assets/Scripts/classObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class classObject
+{
+    static int posX;
+    static int posY;
+    static bool free = true;
+    static int level;
+
+    public classObject(int _posX, int _posY, bool _free, int _level) {
+        posX = _posX;
+        posY = _posY;
+        free = _free;
+        level = _level;
+    }
+}
