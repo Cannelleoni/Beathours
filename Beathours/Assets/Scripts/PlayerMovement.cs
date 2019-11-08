128490db1760d52e7fb561094b57a93b52922498
Leon Portius <Pluto-is-not-too-small@gmx.de>
2019-11-09 00:57:19 +0100

    sound works

--- Beathours/Assets/Scripts/PlayerMovement.cs
+++ Beathours/Assets/Scripts/PlayerMovement.cs
@@ -9,19 +9,36 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] GameObject tower, newParent;
 
+    Vector3 standstill = new Vector3(0 , 0, 0);
+
+    private void Start()
+    {
+        
+    }
 
     private void Update()
-    {
+    {//
         // rechts
-        if(InputManager.mainJoyStick() != null)
+        if(InputManager.mainJoyStick() != standstill)
         {
             rb.MovePosition(transform.position + InputManager.mainJoyStick() * Time.deltaTime * speed);
+            
+            
+                float yAngl = Mathf.Atan2(InputManager.mainHorizontal(), InputManager.mainVertical()); // NEW AND HAS TO STAY
+                yAngl = Mathf.Rad2Deg * yAngl;                                                                                      // rb.transform.Rotate(0,yAngl,0,Space.World); // NEW AND HAS TO STAY
+                print(yAngl);
+                rb.transform.rotation = Quaternion.Euler(0, -yAngl, 0);
+
+
+
+            //rb.transform.rotation = Quaternion.Euler(0, yAngl, 0);
         } 
 
         if(transform.position.y < -2f)
         {
             transform.position = new Vector3(-3.5f, .25f, -2.5f);
         }
+        
 
         
     }
@@ -29,7 +46,7 @@ public class PlayerMovement : MonoBehaviour
     public void changeParent()
     {
         tower.transform.SetParent(newParent.transform);
-        tower.transform.position = new Vector3(transform.position.x, transform.position.y + 1.25f, transform.position.z);
+        tower.transform.position = new Vector3(transform.position.x, transform.position.y + 1.8f, transform.position.z);
     }
 
     //classObject[] coArr = new classObject[400];
