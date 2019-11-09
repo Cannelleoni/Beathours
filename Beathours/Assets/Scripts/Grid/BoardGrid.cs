d183a9b18e92ef192fd8b0960fdeeaee800520ba
Tina <tina.kiessling@t-online.de>
2019-11-09 01:18:30 +0100

    Grid Added
    Finally

---
+++ Beathours/Assets/Scripts/Grid/BoardGrid.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardGrid : MonoBehaviour
+{
+
+    /*   Array consists of integer values.
+     *   0 = empty
+     *   1 = Tower Level 1
+     *   2 = Tower Level 2
+     *   3 = Tower Level 3
+     *   4 = Player Character 
+     */
+    static int[,] grid;
+
+    // On start, all the gridTiles are empty.
+    private void Start() {
+        // makes grid as large as the plane
+        grid = new int[20, 20];
+
+        // z - coordinate
+        for (int z = 0; z < grid.GetLength(1); z++) {
+
+            // x - coordinate
+            for (int x = 0; x < grid.GetLength(0); x++) {
+
+                SetGridTileValue(x, z, 0);
+            }
+        }
+    }
+
+    // returns the whole grid array
+    public static int[,] GetGrid() {
+
+        return grid;
+    }
+
+    // sets value on position x, z to value.
+    public static void SetGridTileValue(int x, int z, int value) {
+        Debug.Log(x);
+        grid[x, z] = value;
+    }
+
+    // returns the value on position x, z.
+    public static int GetGridTileValue(int x, int z) {
+
+        return grid[x, z];
+    }
+
+}
---
+++ Beathours/Assets/Scripts/Tower/TowerBuild.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuild : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    Vector3 playerPosition;
+    [SerializeField] GameObject gridTileGreen, gridTileRed;
+    float lastHorizontal, lastVertical;
+    GameObject currentFocusedTile;
+    Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastHorizontal = 0;
+        lastVertical = 0;
+        currentFocusedTile = Instantiate(gridTileGreen, new Vector3((int)player.transform.position.x, 0, (int)player.transform.position.z), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        markTileAsBuildable();
+    }
+
+    public void markTileAsBuildable() {
+
+        lastHorizontal = InputManager.mainHorizontal();
+        lastVertical = InputManager.mainVertical();
+        playerPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+
+
+        if (lastHorizontal == 0) {
+
+            // above
+            if (lastVertical < 0) {
+
+                currentFocusedTile.transform.position = new Vector3((int)Mathf.Round(playerPosition.x), 0, Mathf.Ceil(playerPosition.z + 1));
+
+                // green
+                if (BoardGrid.GetGridTileValue((int)Mathf.Round(playerPosition.x), (int)Mathf.Ceil(playerPosition.z)) == 0) {
+
+                }
+
+                // red
+                else {
+
+                }
+            }
+            // below
+            else if (lastVertical > 0) {
+
+                currentFocusedTile.transform.position = new Vector3((int)Mathf.Round(playerPosition.x), 0, Mathf.Floor(playerPosition.z - 1));
+
+                //green
+                if (BoardGrid.GetGridTileValue((int)Mathf.Round(playerPosition.x), (int)Mathf.Floor(playerPosition.z - 1)) == 0) {
+
+
+                }
+
+                // red
+                else {
+
+                }
+            }
+        }
+        else if (lastVertical == 0) {
+
+            // right
+            if (lastHorizontal > 0) {
+
+                currentFocusedTile.transform.position = new Vector3(Mathf.Ceil(playerPosition.x + 1), 0, (int)Mathf.Round(playerPosition.z));
+
+                // green
+                if (BoardGrid.GetGridTileValue((int)Mathf.Ceil(playerPosition.x), (int)Mathf.Round(playerPosition.z)) == 0) {
+
+
+                }
+
+                // red
+                else {
+
+                }
+            }
+
+            // left
+            else if (lastHorizontal < 0) {
+
+                currentFocusedTile.transform.position = new Vector3(Mathf.Floor(playerPosition.x - 1), 0, (int)Mathf.Round(playerPosition.z));
+
+                // green
+                if (BoardGrid.GetGridTileValue((int)Mathf.Floor(playerPosition.x - 1), (int)Mathf.Round(playerPosition.z)) == 0) {
+
+                }
+
+                // red
+                else {
+
+                }
+            }
+        }
+    }
+}
